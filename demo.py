2ea35bef3295ebb3829f546c697429efd38785db
Dev1neR <35506504+Dev1neR@users.noreply.github.com>
2021-06-16 16:40:41 +0300

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,23 @@
+#https://www.raspberrypi.org/forums/viewtopic.php?t=253184
+import time
+import pygame
+
+from adafruit_servokit import ServoKit
+from mpu6050 import mpu6050
+sensor = mpu6050(0x68,bus=3)
+kit = ServoKit(channels=16)
+
+while(True):
+    gyro_data = sensor.get_gyro_data()
+    if (gyro_data['x'] > 3):
+        kit.servo[].angle = ;
+        kit.servo[].angle = ;
+    elif (gyro_data['x'] < -3):
+        kit.servo[].angle = ;
+        kit.servo[].angle = ;
+    elif (gyro_data['y'] > 3):
+        kit.servo[].angle = ;
+        kit.servo[].angle = ;
+    elif (gyro_data['y'] < -3):
+        kit.servo[].angle = ;
+        kit.servo[].angle = ;
