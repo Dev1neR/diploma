c8560eb4762cebf0b738a5ba507417c1c4b6332d
Dev1neR <35506504+Dev1neR@users.noreply.github.com>
2021-06-18 16:42:36 +0300

    Update main3.py

--- main3.py
+++ main3.py
@@ -5,6 +5,17 @@ import pygame
 from adafruit_servokit import ServoKit
 from mpu6050 import mpu6050
 
+def dist(a,b):
+    return math.sqrt((a*a)+(b*b))
+
+def get_y_rotation(x,y,z):
+    radians = math.atan2(x, dist(y,z))
+    return -math.degrees(radians)
+
+def get_x_rotation(x,y,z):
+    radians = math.atan2(y, dist(x,z))
+    return math.degrees(radians)
+
 sensor = mpu6050(0x68,bus=3)
 kit = ServoKit(channels=16)
 
@@ -28,14 +39,16 @@ def menu(x):
     print("forward")   
     kit.servo[2].angle = 87
     kit.servo[3].angle = 70
-    kit.servo[0].angle = 170
-    kit.servo[1].angle = 170
-    if (gyro_data['x'] > 3):
+    if (xaxis > 3):
+        kit.servo[0].angle = 180
+        kit.servo[1].angle = 170
         kit.servo[6].angle = 170
         kit.servo[7].angle = 170
     else:
-        kit.servo[6].angle = 60
-        kit.servo[7].angle = 60
+        kit.servo[0].angle = 140
+        kit.servo[1].angle = 140
+        kit.servo[6].angle = 130
+        kit.servo[7].angle = 130
   elif x == "s":
     print("backwards")
     kit.servo[2].angle = 87
@@ -73,8 +86,11 @@ screen = pygame.display.set_mode((400,400))
 
 done = False
 while not done:
-    gyro_data = sensor.get_gyro_data()
-    print(gyro_data['x'])
+    gyro_data = sensor.get_accel_data()
+    xaxis = get_x_rotation(gyro_data['x'], gyro_data['y'], gyro_data['z'])
+    yaxis = get_y_rotation(gyro_data['x'], gyro_data['y'], gyro_data['z'])
+    print("X Rotation: ", xaxis)
+    print("Y Rotation: ", yaxis)
     time.sleep(0.2)
 
     for event in pygame.event.get():
